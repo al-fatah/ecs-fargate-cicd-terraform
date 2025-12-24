c005b5cd2d81512ab49c0deb08f6bde1eabed8a7
al-fatah <alfatah.din@gmail.com>
2025-12-24 14:13:11 +0800

    test: trigger deploy

--- app/server.js
+++ app/server.js
@@ -6,3 +6,5 @@ app.get("/health", (req, res) => res.status(200).send("ok"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on ${port}`));
+
+// For testing locally
