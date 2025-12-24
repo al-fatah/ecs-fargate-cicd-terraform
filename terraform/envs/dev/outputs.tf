75195cc8d298cc30ddbc3a74b1f4d602cfd03b21
al-fatah <alfatah.din@gmail.com>
2025-12-24 13:55:26 +0800

    Phase 2: terraform dev infra (ecr, ecs fargate, alb, logs)

---
+++ terraform/envs/dev/outputs.tf
@@ -0,0 +1,19 @@
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.this.name
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.app.name
+}
+
+output "task_definition_family" {
+  value = aws_ecs_task_definition.app.family
+}
+
+output "alb_dns_name" {
+  value = aws_lb.app.dns_name
+}
